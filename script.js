1f31f755fedd2c750a535b53d85c1c965715f05f
DavidVinent <dvinent20628@iesjoanramis.org>
2025-10-03 08:49:43 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -16,11 +16,33 @@ function Canvifoto(){
 
 function invis() {
     if(document.getElementById("foto").hidden == true){
-        document.getElementById("foto").hiden = false
+        document.getElementById("foto").hidden = false
     }else{
         document.getElementById("foto").hidden = true
     }
 
 
 }
+
+
+function Canvifons() {
+   let body = document.getElementsByTagName("body")[0]
+   let foto = prompt("Donde quieres viajar")
+   body.style = "background-image: url('"+foto+"'); background-size: cover;";
+
+}
+
+function Tradueix() {
+    let body = document.getElementsByTagName("body")[0];
+    body.style = "font-family: 'Times New Roman', Times, serif;"
+}
+
+function Info() {
+    let llista = document.getElementById("expL");
+    let element = document.createElement("li");
+    element.innerHTML = prompt("Que vols afegir")
+    llista.appendChild(element)
+}
+
+
 
